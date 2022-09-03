6bc1d925b7ac473f63021194eefd3208dcb0b14e
jorgeturriate <jorge_xt@hotmail.com>
2022-09-03 05:49:43 +0000

    pairController

--- nodejs/code/models/pair.js
+++ nodejs/code/models/pair.js
@@ -1,4 +1,23 @@
 //Importing binance
 const binance= require('../config/binance.js');
+/* const axios= require('axios').default; */
 
-//holi
+//let test= await binance.prices();
+
+const pair= class Pair {
+    constructor(name, coins){
+        this.name;
+        this.coins
+    }
+
+    //Getters
+    get name() {return this.name}
+    get coins() {return this.coins}
+
+
+
+}
+
+module.exports= pair;
+
+
